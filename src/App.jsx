ac4978b94ff5bd7a9ce9eb4ea0baea84b2e0cf14
Oleksii Korolenko <as_vostok@i.ua>
2023-05-23 20:03:54 +0300

    added: fix App

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
 import { lazy } from "react";
 import { Route, Routes } from "react-router-dom";
 
-import { Layout } from "./Layout/Layout";
+import { Layout } from "./components/Layout/Layout";
 
 const NotFound = lazy(() =>
-  import("../pages/NotFound").then((res) => ({
+  import("./pages/NotFound").then((res) => ({
     default: res.NotFound,
   }))
 );
 
 const HomePage = lazy(() =>
-  import("../pages/HomePage").then((res) => ({
+  import("./pages/HomePage").then((res) => ({
     default: res.HomePage,
   }))
 );
 
 const TweetsPage = lazy(() =>
-  import("../pages/TweetsPage").then((res) => ({
+  import("./pages/TweetsPage").then((res) => ({
     default: res.TweetsPage,
   }))
 );
--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { PersistGate } from "redux-persist/es/integration/react";
 import { persistor, store } from "./redux/store";
 import { ThemeProvider } from "@emotion/react";
 
-import App from "./components/App";
+import App from "./App";
 
 import { theme } from "./theme";
 import "./index.css";
